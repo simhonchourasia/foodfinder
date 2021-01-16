f446f17ef2189a8f21baf0c2cb3d45e64a901e1b
Mibby123 <67397927+Mibby123@users.noreply.github.com>
2021-01-16 18:24:39 -0500

    Outputs csv
    Sample location with example csv

--- filter.js
+++ filter.js
@@ -1,45 +1,27 @@
+//npm install csvtojson
+//npm install csv-string
 const request = require('request');
 const fetch = require('node-fetch');
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
+const { stringify } = require('csv-string');
+const csvtojson = require('csvtojson')
 
-// const options = {
-//   method: 'POST',
-//   url: 'https://yelpapiserg-osipchukv1.p.rapidapi.com/getBusinesses',
-//   headers: {
-//     'content-type': 'application/x-www-form-urlencoded',
-//     'x-rapidapi-key': '3e1b2561demshd0398fd2d749e71p1b2dd3jsnea2c6415e0ff',
-//     'x-rapidapi-host': 'YelpAPIserg-osipchukV1.p.rapidapi.com',
-//     useQueryString: true
-//   },
-//   form: {
-//     term: 'restaurants',
-//     accessToken: 'R-v4bb0uqTia9h_S5cC5-v4cy3JTcqPaEcFoZ26WjmheQiOsh0Brc-QT0kiMfX_nc-PePBM4ZiPpdyVrrcSdwmrty0y3D94hn2N92b0mPx3OeOI__ZMjl1bjVnkCYHYx',
-//     radius: '10000',
-//     location: 'Toronto',
-//     limit: '20'
-//   }
-// };
-
-// request(options, function (error, response, body) {
-// 	if (error) throw new Error(error);
-//   console.log("Hello")
-// 	console.log(body);
-// });
-
-async function test() {
+async function rest_data() {
 
     //LOCATION:
-    address = '';//Some user input string
-
+    address = '5995 Steeles Ave E, Scarborough, ON, CA';//Some user input string
+    for (var i = 0; i < address.length; i++) {
+        if (address[i] == ' ') {
+            address[i] == '+';
+        }
+    }
     //const location = await fetch()
 
-    main_url = 'https://api.yelp.com/v3/businesses/search?term&latitude=43.6532&longitude=-79.3832';
-    url2 = 'https://api.yelp.com/v3/businesses/search?term&radius=10000&location=' + address;
-    // const response = await fetch(main_url,
-    //     { method: 'GET', headers: { Authorization: "Bearer R-v4bb0uqTia9h_S5cC5-v4cy3JTcqPaEcFoZ26WjmheQiOsh0Brc-QT0kiMfX_nc-PePBM4ZiPpdyVrrcSdwmrty0y3D94hn2N92b0mPx3OeOI__ZMjl1bjVnkCYHYx" } });
-    // const json = await response.json();
+    def_url = 'https://api.yelp.com/v3/businesses/search?term&latitude=43.6532&longitude=-79.3832';
+    url = 'https://api.yelp.com/v3/businesses/search?term&radius=10000&location=' + address;
 
-    const response = await fetch(url2, 
-    {method: 'GET', headers: { Authorization: "Bearer R-v4bb0uqTia9h_S5cC5-v4cy3JTcqPaEcFoZ26WjmheQiOsh0Brc-QT0kiMfX_nc-PePBM4ZiPpdyVrrcSdwmrty0y3D94hn2N92b0mPx3OeOI__ZMjl1bjVnkCYHYx"}});
+    const response = await fetch(url,
+        { method: 'GET', headers: { Authorization: "Bearer R-v4bb0uqTia9h_S5cC5-v4cy3JTcqPaEcFoZ26WjmheQiOsh0Brc-QT0kiMfX_nc-PePBM4ZiPpdyVrrcSdwmrty0y3D94hn2N92b0mPx3OeOI__ZMjl1bjVnkCYHYx" } });
     const json = await response.json();
 
     const restaurants = JSON.stringify(json);
@@ -53,7 +35,7 @@ async function test() {
     var price = new Array(business.length);
     var catagories = new Array(business.length);
     var image_url = new Array(business.length);
-    
+
     //Filtering out useless data
     for (var i = 0; i < business.length; i++) {
         restaurant_name[i] = business[i].name;
@@ -61,22 +43,35 @@ async function test() {
         rating[i] = business[i].rating;
         address[i] = business[i].location.address1;
         price[i] = business[i].price;
-        catagories[i] = business[i].catagories;
+        //catagories[i] = business[i].catagories;
         image_url[i] = business[i].image_url;
     }
+
+    //console.log(business[0].catagories);
 
     //Filtering out businesses that dont meet standard
     for (i = 0; i < rating.length; i++) {
         if (rating[i] < 2.5 || is_closed[i] == 'true') {
-            restaurant_name.splice(i, 1);
-            is_closed.splice(i, 1);
-            rating.splice(i, 1);
-            address.splice(i, 1);
-            price.splice(i, 1);
-            catagories.splice(i, 1);
-            image_url.splice(i,1);
+            restaurant_name.splice(i, 1); // string
+            is_closed.splice(i, 1);// string
+            rating.splice(i, 1); // double
+            address.splice(i, 1); // string
+            price.splice(i, 1); // string
+            //catagories.splice(i, 1); // array of strings
+            image_url.splice(i, 1); // string
             i -= 1;
         }
-    } 
+    }
+    //Converting arrays into one CSV array
+    var csvContent = 'restaurant name,rating,address,price,image_url\r\n';
+    for (i = 0; i < rating.length; i++) {
+        var row = restaurant_name[i] + ',' + rating[i].toString() + ',' + address[i] + ',' + price[i] + ',' + image_url[i];
+        csvContent += row + '\r\n';
+    }
+
+    //https://www.npmjs.com/package/csv-string
+    const CSV = require('csv-string');
+    const parsedCsv = CSV.parse(csvContent);
+    console.log(parsedCsv);
 }
-test();
+rest_data();
