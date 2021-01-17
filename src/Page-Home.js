49c1cf844c7136c78a610683a26255c6e03d2159
AngelaDu <38049339+AngelaDu@users.noreply.github.com>
2021-01-17 03:50:34 -0500

    Update Page-Home.js

--- src/Page-Home.js
+++ src/Page-Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import TinderCards from "./TinderCards";
 import SwipeButtons from "./SwipeButtons";
+import "./App.css";
 
 function Home() {
     return (
-        <div className= 'home'>
-            <TinderCards/>
-            <SwipeButtons/>  {/*  */}
-        </div>
+            <div className= 'home'>
+                <TinderCards/>
+                 {/* <SwipeButtons/>  */}
+            </div>
     )
 }
 
