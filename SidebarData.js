7af159e35037583444ad82f0a7b1627795ede9b2
quarterdill <46429171+quarterdill@users.noreply.github.com>
2021-01-17 03:20:43 -0500

    Changed logout path and login authentication

---
+++ SidebarData.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import * as AiIcons from "react-icons/ai";
+
+/* UPDATE THIS TO ADD MORE OPTIONS IN THE SIDEBAR*/
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Matches',
+        path: '/matches',
+        icon: <AiIcons.AiFillApi />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Login',
+        path: '/login',
+        icon: <AiIcons.AiOutlineLogout />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Logout',
+        path: '/login',
+        icon: <AiIcons.AiOutlineLogout />,
+        cName: 'nav-text'
+    }
+]
