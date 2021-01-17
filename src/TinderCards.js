e0d5139d27e019b273cec49d64a3c23cd889b7da
AngelaDu <38049339+AngelaDu@users.noreply.github.com>
2021-01-17 03:51:56 -0500

    Update TinderCards.js

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -3,6 +3,10 @@ import {Link} from 'react-router-dom';
 import axios from 'axios';
 import TinderCard from "react-tinder-card";
 import './TinderCards.css';
+import './SwipeButtons.css';
+import * as Io5Icons from "react-icons/io5";
+import * as IoIcons from "react-icons/io";
+import { IconButton } from '@material-ui/core';
 
 export default class TinderCards extends Component {
     // const [ restaurants ] = useState([
@@ -15,9 +19,9 @@ export default class TinderCards extends Component {
         this.swipeLeft = this.swipeLeft.bind(this);
         this.swipeRight = this.swipeRight.bind(this);
         // HARDCODED userId (username: Albert)
-        this.state = {userId: '6003c1d0f391200b6cfaa7dc', items: []};
+        //this.state = {userId: '6003c1d0f391200b6cfaa7dc', items: []};
         // HARDCODED userId (username: Barbara)
-        //this.state = {userId: '6003c1e5f391200b6cfaa7dd', items: []};
+        this.state = {userId: '6003c1e5f391200b6cfaa7dd', items: []};
     }
 
     componentDidMount() {
@@ -71,25 +75,39 @@ export default class TinderCards extends Component {
     };
 
     render() {return (
-        <div className='tinder-card'>
-            <div className="tinderCards__cardContainer">
-            {this.state.items.map((item)=> (
-                <TinderCard
-                className="swipe"
-                key={item.name}
-                preventSwipe={["up", "down"]}
-                onSwipe = {(dir) => this.swiped(dir, item._id, item.restaurant_name)}
-                onCardLeftScreen={() => this.outOfFrame(item.restaurant_name)}
-                >
-                    <div 
-                    style = {{backgroundImage: `url(${item.image_url})` }}
-                    className="card">
-                        <h3>{item.restaurant_name}</h3>
-                        <p className='description'>{item.rating}</p>
-                    </div>
+        <div className='HomePage'>
+            <div className='tinder-card'>
+                <div className="tinderCards__cardContainer">
+                {this.state.items.map((item)=> (
+                    <TinderCard
+                    className="swipe"
+                    key={item.name}
+                    preventSwipe={["up", "down"]}
+                    onSwipe = {(dir) => this.swiped(dir, item._id, item.restaurant_name)}
+                    onCardLeftScreen={() => this.outOfFrame(item.restaurant_name)}
+                    >
+                        <div 
+                        style = {{backgroundImage: `url(${item.image_url})` }}
+                        className="card">
+                            <h3>{item.restaurant_name}</h3>
+                            <p className='description'>{item.rating}</p>
+                        </div>
 
-                </TinderCard>
-            ))}
+                    </TinderCard>
+                ))}
+                </div>
+            </div>
+            <div className="swipeButtons">
+                <IconButton className="swipeButtons__left">
+                    <div className="theX">
+                        <Io5Icons.IoClose />
+                    </div>
+                </IconButton>
+                <IconButton className="swipeButtons__right">
+                    <div className="theHeart">
+                        <IoIcons.IoIosHeart style={{fill: '#55c16e'}}/>
+                    </div>
+                </IconButton>
             </div>
         </div>
     )}
